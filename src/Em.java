02f4a530fcd3bf9ba377bebfa2598ead9bc35e73
admin <ss>
2020-06-19 08:28:37 +0800

    111

---
+++ src/Em.java
@@ -0,0 +1,11 @@
+public enum Em {
+
+    MALE(1),
+    ROLAE(2),
+    ;
+    private int key;
+
+    Em(int key) {
+        this.key = key;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test<E> {
+
+    static final String MATE = "1";
+    static final String ROMATE = "0";
+    static final String c = "a";
+    static final String d = "a";
+    enum CLASS {
+        CLASS_1, CLASS_2, CLASS_3
+    }
+
+    public void m1(Em p1, String p2) {
+        if (p1.equals(Em.MALE)) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        List ls = new ArrayList<>();
+
+        ls.add("@@@@");
+    }
+}
